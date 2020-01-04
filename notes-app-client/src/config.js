06ca36198364770937e20152f66adc9ea74dd8a9
Aadi Shlok <shlok.a@husky.neu.edu>
2020-01-04 02:08:14 -0500

    Setting up the react app

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-2",
+        BUCKET: "notes-app-api-prod-serverlessdeploymentbucket-18u2c3k5880uc"
+    },
+    apiGateway: {
+        REGION: "us-east-2",
+        URL: "https://eyaj45gapf.execute-api.us-east-2.amazonaws.com/prod/"
+    },
+    cognito: {
+        REGION: "us-east-2",
+        USER_POOL_ID: "us-east-2_XPWVMPLTd",
+        APP_CLIENT_ID: "5li52rggpjupk0vm2o9mvlhj38 ",
+        IDENTITY_POOL_ID: "us-east-2:4f4ebb13-bdfd-489f-b458-820d6824279e"
+    }
+};
